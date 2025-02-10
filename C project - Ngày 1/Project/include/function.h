2ea1617e27a16fa4e4c1d1619404654bb3ab3069
HaoLe-cyber <lehuunhathao2k6@gmail.com>
2025-02-10 10:26:55 +0700

    C project Ngày 1

---
+++ C project - Ngày 1/Project/include/function.h
@@ -0,0 +1,14 @@
+// Khai báo hàm
+
+// Hàm hiển thị menu chính
+void mainMenu();
+// Hàm hiển thị menu quản lý lớp học
+void classManagementMenu();
+// Hàm hiển thị danh sách lớp
+void classlist();
+// Hàm thêm lớp mới
+void addNewClass();
+// Hàm chỉnh sửa thông tin lớp học
+void editClassInformation();
+// Hàm xóa lớp học
+void deleteClass();
